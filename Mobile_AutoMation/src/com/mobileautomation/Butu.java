0410097a71f2182b900e82e03240b2c02fe1903b
Nitya <nitya.ranjan101991@gmail.com>
2017-04-05 01:02:56 +0530

    update butu

--- Mobile_AutoMation/src/com/mobileautomation/Butu.java
+++ Mobile_AutoMation/src/com/mobileautomation/Butu.java
@@ -2,4 +2,10 @@ package com.mobileautomation;
 
 public class Butu {
 //banda
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("good boy");
+
+	}
+
 }
